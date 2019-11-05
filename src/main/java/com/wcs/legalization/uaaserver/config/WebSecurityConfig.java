3cf116d5e3eaa22cd7ca0e1a2c88abaca50828ea
Muluken <muluken@berkeley.edu>
2019-11-04 22:09:13 -0500

    tls certificate added and http gets redirected to https

--- src/main/java/com/wcs/legalization/uaaserver/config/WebSecurityConfig.java
+++ src/main/java/com/wcs/legalization/uaaserver/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @Order(2)
@@ -15,10 +16,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 			.authenticated()
 			.and()
-			.httpBasic()
+			.formLogin()
 			.and()
-			.logout();
+			.csrf()
+			.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()); // for single page apps
 	}
-
-	
 }
